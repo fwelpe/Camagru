ae30e70110ebf85937e26269ac22829e26661cfa
fwelpe <arestovfa@gmail.com>
2019-07-04 19:57:39 +0300

    pMA

--- UploadContent.php
+++ UploadContent.php
@@ -2,10 +2,10 @@
 
 try {
 	# MySQL через PDO_MYSQL
-	$DBH = new PDO("mysql:host=localhost;dbname=db", "root", "");
+	$DBH = new PDO("mysql:host=localhost;dbname=db", "root", "Rerfderf5");
 	}
 	catch(PDOException $e) {
 		echo $e->getMessage();
 	}
-
+echo "OK";
 ?>
