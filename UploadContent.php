e098528175cca612aca85347deeca3bacc84abe8
fwelpe <arestovfa@gmail.com>
2019-07-07 13:35:40 +0300

    sync

--- UploadContent.php
+++ UploadContent.php
@@ -2,7 +2,7 @@
 
 try {
 	# MySQL через PDO_MYSQL
-	$DBH = new PDO("mysql:host=localhost;dbname=db", "root", "Rerfderf5");
+	$DBH = new PDO("mysql:host=localhost;dbname=db", "root", "ewe4");
 	}
 	catch(PDOException $e) {
 		echo $e->getMessage();
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 	</head>
 	<body>
 		<h1>sup H1</h1>
@@ -13,5 +14,24 @@
 			File:
     		<input type="file" name="image"> <input type="submit" value="Upload">
 		</form>
+		<div id="my_camera"></div>
+		<input type=button value="Take Snapshot" onClick="take_snapshot()">
+		<script language="JavaScript">
+    Webcam.set({
+        width: 490,
+        height: 390,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+  
+    Webcam.attach( '#my_camera' );
+  
+    function take_snapshot() {
+        Webcam.snap( function(data_uri) {
+            $(".image-tag").val(data_uri);
+            document.getElementById('results').innerHTML = '<img src="'+data_uri+'"/>';
+        } );
+    }
+</script>
 	</body>
 </html>
