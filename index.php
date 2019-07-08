2be5666709045fafb214df473b38c4025b9ea869
Crawford Denys arryn <cdenys-a@oa-e4.21-school.ru>
2019-07-08 19:34:01 +0300

    sync

--- index.php
+++ index.php
@@ -1,41 +1,45 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
-		<h1>sup H1</h1>
-		<?php print "echo from PHP\n" ?>
-		<br />
-		<br />
-		<br />
-		<form method="POST" action="storeImage.php">
-			<input type="file" accept="image/*;capture=camera" name="qwe">
-			<input type="submit">
-		</form>
 
-		<video autoplay></video>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 
-<script>
-  var onFailSoHard = function(e) {
-    console.log('Reeeejected!', e);
-  };
+<body>
+	<header>
 
-  // Not showing vendor prefixes.
-  navigator.getUserMedia('video', function(localMediaStream) {
-    var video = document.querySelector('video');
-    video.src = window.URL.createObjectURL(localMediaStream);
+	</header>
+	<h1>sup. H1</h1>
+	<?php echo "echo from PHP" ?>
+	<br />
+	<br />
+	<br />
+	<canvas id="canvas" width="150" height="75"></canvas>
+	<video id="video" autoplay width="128" height="72"></video>
+	<script>
+		// Prefer camera resolution nearest to 1280x720.
+		var constraints = {
+			video: true
+		};
 
-    // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-    // See crbug.com/110938.
-    video.onloadedmetadata = function(e) {
-      // Ready to go. Do some stuff.
-    };
-  }, onFailSoHard);
-  navigator.webkitGetUserMedia('audio, video', function(localMediaStream) {
-  var video = document.querySelector('video');
-  video.src = window.webkitURL.createObjectURL(localMediaStream);
-}, onFailSoHard);
-</script>
-	</body>
+		navigator.mediaDevices.getUserMedia(constraints)
+			.then((mediaStream) => {
+				var video = document.querySelector('video');
+				video.srcObject = mediaStream;
+				video.onloadedmetadata = function(e) {
+					video.play();
+				};
+			})
+			.catch(function(err) {
+				console.log(err.name + ": " + err.message);
+			});
+		const canvas = document.getElementById("canvas");
+		const context = canvas.getContext("2d");
+		video = document.getElementById("video");
+		canvas.addEventListener("click", function() {
+			context.drawImage(video, 0, 0, 128, 72);
+		})
+	</script>
+</body>
+
 </html>
--- storeImage.php
+++ storeImage.php
@@ -1,11 +1,8 @@
 <?php
 
-if ($_POST["qwe"] && )
+if ($_POST["qwe"])
 {
 	echo $_POST["qwe"];
-	move_uploaded_file($_POST["qwe"],"images/"."test")
 }
 else
 	echo 1;
-
-?>
