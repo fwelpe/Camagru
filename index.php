be6d48badfbdb208ebd93ccb80a117749f7adac5
Crawford Denys arryn <cdenys-a@il-i6.21-school.ru>
2019-07-09 17:29:40 +0300

    sync

--- index.php
+++ index.php
@@ -2,42 +2,43 @@
 <html>
 
 <head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/w3.css">
 </head>
 
-<body>
+<body bgcolor="f2f1f3">
 	<header>
-
+		<div class="w3-container w3-blue">
+			<div>asd</div>
+			<p>The w3-container class is an important w3.CSS class.</p>
+		</div>
 	</header>
 	<h1>sup. H1</h1>
 	<?php echo "echo from PHP" ?>
 	<br />
 	<br />
 	<br />
-	<canvas id="canvas" width="150" height="75"></canvas>
-	<video id="video" autoplay width="128" height="72"></video>
+	<canvas id="canvas"></canvas>
+	<video id="video" autoplay></video>
 	<script>
-		// Prefer camera resolution nearest to 1280x720.
-		var constraints = {
+		const video = document.querySelector('video');
+		const canvas = document.getElementById("canvas");
+		const context = canvas.getContext("2d");
+		const constraints = {
 			video: true
 		};
 
 		navigator.mediaDevices.getUserMedia(constraints)
 			.then((mediaStream) => {
-				var video = document.querySelector('video');
 				video.srcObject = mediaStream;
-				video.onloadedmetadata = function(e) {
+				video.onloadedmetadata = () => {
 					video.play();
 				};
 			})
-			.catch(function(err) {
+			.catch((err) => {
 				console.log(err.name + ": " + err.message);
 			});
-		const canvas = document.getElementById("canvas");
-		const context = canvas.getContext("2d");
-		video = document.getElementById("video");
-		canvas.addEventListener("click", function() {
-			context.drawImage(video, 0, 0, 128, 72);
+		video.addEventListener("click", () => {
+			context.drawImage(video);
 		})
 	</script>
 </body>
