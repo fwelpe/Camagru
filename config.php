ed9a7bda2291712beaeb5e80b4289302743b1d87
Crawford Denys arryn <cdenys-a@il-i3.21-school.ru>
2019-07-16 21:44:14 +0300

    upload page adding

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$server = "95.165.129.236";
+$username = "root";
+$password = "ewe4";
+$dbname = "gru";
+
+// Create connection
+try{
+  $conn = new PDO("mysql:host=$server;dbname=$dbname","$username","$password");
+  $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  die('Unable to connect with the database');
+}
--- index.php
+++ index.php
@@ -3,20 +3,15 @@
 
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="css/w3.css" />
 	<link rel="stylesheet" href="css/main.css" />
 </head>
 
 <body bgcolor="f2f1f3">
 	<?php include("header.html") ?>
-	<h1>sup. H1</h1>
-	<?php echo "echo from PHP" ?>
-	<br />
-	<br />
-	<br />
 	<canvas id="canvas"></canvas>
-	<video id="video" autoplay></video>
+	<video id="video" autoplay width="640" height="480"></video>
 	<script>
 		const video = document.querySelector('video');
 		const canvas = document.getElementById("canvas");
@@ -37,8 +32,15 @@
 			});
 		video.addEventListener("click", () => {
 			context.drawImage(video, 0, 0, canvas.width, canvas.height);
-		})
+		});
 	</script>
+
+	<img src="stickers/src/file_3567088.webp" alt="WebP rules." />
+
+	<form method='post' action='up.php' enctype='multipart/form-data'>
+  <input type='file' name='files[]' multiple />
+  <input type='submit' value='Submit' name='submit' />
+</form>
 </body>
 
 </html>
