9fea756f87afe70a7b50ddf9f14feab6f62f1f6c
fwelpe <arestovfa@gmail.com>
2019-08-16 00:13:30 +0300

    sync

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (!$_SESSION["user"])
+	header("Location: login.php")
+?>
+
 <!DOCTYPE HTML>
 <html>
 
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset='utf-8'>
+	<title>Camagru: Login</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+</head>
+
+<body>
+	<form method="POST" action=".">
+		<input type="text" name="login" value="" placeholder="Username" />
+		<br />
+		<input type="password" name="passwd" value="" placeholder="Password" />
+		<br />
+		<input type="submit" name="submit" value="OK" />
+	</form>
+
+</body>
+
+</html>
